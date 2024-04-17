fc9bbdf03bba6338185f4c47c3c739a73410e8b5
obiaggi <rafaelrocha.biagi@outlook.com>
2024-04-17 09:57:44 -0300

    Add route

---
+++ calculate_shipping/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import path from 'path';
+import { CalculateComponent } from '../modules/calculate/component/calculate.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: CalculateComponent,
+    pathMatch: 'full',
+  },
+];
