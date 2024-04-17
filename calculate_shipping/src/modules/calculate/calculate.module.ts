d0019eca2eded0760db559f2f7569b1271bfcb15
obiaggi <rafaelrocha.biagi@outlook.com>
2024-04-17 09:57:32 -0300

    Add calculate component

---
+++ calculate_shipping/src/modules/calculate/calculate.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalculateComponent } from './component/calculate.component';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+})
+export class CalculateModule {}
