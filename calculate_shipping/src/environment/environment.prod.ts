c586a9ca0e691d08da88161c0962948d862a7eb0
obiaggi <rafaelrocha.biagi@outlook.com>
2024-04-17 09:57:02 -0300

    Add environment

---
+++ calculate_shipping/src/environment/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiDistanceMatrixAccurateUrl:
+    'https://api.distancematrix.ai/maps/api/distancematrix/',
+  apiGeocodingAccurateUrl:
+    'https://api-v2.distancematrix.ai/maps/api/distancematrix/',
+};
---
+++ calculate_shipping/src/environment/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiDistanceMatrixAccurateUrl:
+    'https://api.distancematrix.ai/maps/api/distancematrix/',
+  apiGeocodingAccurateUrl:
+    'https://api-v2.distancematrix.ai/maps/api/distancematrix/',
+};
